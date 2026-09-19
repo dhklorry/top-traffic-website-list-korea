5038b28ac46c03fd5ef52a5633481abaa4f94d5e
Irvin <irvinfly@gmail.com>
2026-09-19 10:16:30 +0900

    Add SimilarWeb Korea traffic source

--- fetch-similarweb.js
+++ fetch-similarweb.js
@@ -12,7 +12,7 @@ const fs = require('fs');
 const { URL } = require('url');
 const zlib = require('zlib');
 
-const country = process.argv[2] || 'south-korea';
+const country = process.argv[2] || 'korea-republic-of';
 const SIMILARWEB_URL = `https://www.similarweb.com/top-websites/${country}/`;
 const OUTPUT_FILE = `similarweb_top_${country}.json`;
 
